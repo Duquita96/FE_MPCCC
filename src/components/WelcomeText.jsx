81e7f9fdbc0b680a1f1299748d507640309fe8f5
maxi <Maximilian.Autrum42@gmx.de>
2024-04-16 15:53:39 +0200

    16_04_2023 update

---
+++ src/components/WelcomeText.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "../style/index.css";
+import "../style/WelcomeText.css";
+
+
+const WelcomeText = () => {
+
+    return (
+        <div className="Welcome">
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci ullam atque minima incidunt. Obcaecati in nesciunt itaque veniam mollitia aspernatur tempore eligendi hic omnis similique, perspiciatis quasi id deserunt voluptatum?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero minus sequi veniam eos soluta, saepe quia quos architecto! Vero, atque animi facilis eaque quis quisquam nostrum quae accusamus dicta ex. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque nam cupiditate voluptatum magni, non aut! Error blanditiis quae deserunt distinctio quam assumenda consequatur quisquam eligendi quidem exercitationem facilis, commodi reiciendis.
+        </p>
+        </div>
+    );
+};
+
+
+export default WelcomeText;
