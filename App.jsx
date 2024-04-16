56cc4f53627347593a41858919b9c6ba92a9bd3c
Claudia <duquita96@gmail.com>
2024-04-16 10:58:30 +0200

    added general css to index (pieter contribution)

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import './src/style/App.css'
+import CardsSection from './src/components/CardsSections.jsx'
+
+function App() {
+
+
+  return (
+    <>
+<CardsSection/>
+    </>
+  )
+}
+
+export default App
