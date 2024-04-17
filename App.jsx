4b66cef7a3eb634090916c2204380e7ed81dfac8
Claudia <duquita96@gmail.com>
2024-04-17 16:32:27 +0200

    fixed typo CardsSections to CardsSection

--- App.jsx
+++ App.jsx
@@ -1,7 +1,7 @@
 import './src/style/App.css'
-import CardsSection from './src/components/CardsSections.jsx'
+import CardsSection from './src/components/CardsSection.jsx'
 import Footer from './src/components/Footer.jsx'
-import WelcomeText from "./src/components/WelcomeText"
+import WelcomeText from './src/components/WelcomeText'
 
 function App() {
 
