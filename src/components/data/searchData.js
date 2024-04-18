163291d855a53f49033bf215f058a1561c492684
Pieter <135125619+PieterDCI@users.noreply.github.com>
2024-04-18 13:24:54 +0200

    add basic main header with search bar

---
+++ src/components/data/searchData.js
@@ -0,0 +1,15 @@
+const data = {
+    // books
+    "The comedy book name": {type: "Book"},
+    "The thriller book name": {type: "Book"},
+
+    // book genres
+    Comedy: {type: "Genre"},
+    Thriller: {type: "Genre"},
+
+    // categories
+    Books: {type: "Category"},
+    Games: {type: "Category"},
+}
+
+export default data;
