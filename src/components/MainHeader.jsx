a38154a7a998e5178b8e437e99baf55b58852e2b
Pieter <135125619+PieterDCI@users.noreply.github.com>
2024-04-18 14:26:31 +0200

    add searchbar component

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -1,51 +1,14 @@
-import { useState } from "react";
 import "../style/headerStyle.css";
-import dataObj from "./data/searchData.js"
+import SearchBar from "./Searchbar";
 
 const MainHeader = () => {
-  const items = Object.keys(dataObj);
-  const [inputStr, setInputStr] = useState("");
-
-  const handleChange = (e) => {
-    setInputStr(e.target.value);
-  };
-  const clear = () => {
-    setInputStr("");
-  };
-
   return (
     <div className="main-header headers">
       <div className="box-cart-login">logo</div>
       <div className="box-cart-login">user</div>
-      <div className="searchbardiv">
-        <form action="">
-          <input
-            className="searchbar"
-            type="search"
-            placeholder="What are you looking for?"
-            onFocus={clear}
-            onChange={handleChange}
-            value={inputStr}
-          />
-        </form>
-      </div>
+      <SearchBar />
       <div className="box-cart-login">cart</div>
       <div className="box-cart-login">login</div>
-      <div className="search-result">
-        {items
-          .filter((item) => {
-            return inputStr.toLowerCase() === ""
-              ? ""
-              : item.toLowerCase().includes(inputStr.toLowerCase());
-          })
-          .map((item, key) => {
-            return (
-              <li key={key}>
-                {item} <span className="searchType">{dataObj[item].type}</span>
-              </li>
-            );
-          })}
-      </div>
     </div>
   );
 };
