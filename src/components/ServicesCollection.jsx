1dd7592423af9aa56b1bd3a3b74f22d105f35cd4
Claudia <duquita96@gmail.com>
2024-04-22 16:59:23 +0200

    added responsivness to service-collection

--- src/components/ServicesCollection.jsx
+++ src/components/ServicesCollection.jsx
@@ -7,18 +7,14 @@ import { useEffect, useState } from 'react';
 export const ServicesCollection= () => {
   const [randomData, setRandomData] = useState([]);
 
-  // Función para mezclar los datos
+  // Shuffle data
   const shuffle = (array) => {
     let currentIndex = array.length, temporaryValue, randomIndex;
-
-    // Mientras queden elementos para mezclar...
     while (0 !== currentIndex) {
-
-      // Selecciona un elemento sin mezclar...
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex -= 1;
 
-      // E intercambia con el elemento actual
+      // change the actual element for the new one
       temporaryValue = array[currentIndex];
       array[currentIndex] = array[randomIndex];
       array[randomIndex] = temporaryValue;
@@ -28,7 +24,7 @@ export const ServicesCollection= () => {
   }
 
   useEffect(() => {
-    setRandomData(shuffle(toursData.data).slice(0, 6)); // Mezcla los datos y toma los primeros 5
+    setRandomData(shuffle(toursData.data).slice(0, 6)); // Shuffle the data and take the first one
   }, []);
 
   return (
