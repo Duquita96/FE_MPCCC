f96ddb27d01379166de74f229aae8bac7b196d5a
maxi <Maximilian.Autrum42@gmx.de>
2024-04-22 11:23:00 +0200

    update

--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -1,34 +1,53 @@
 import "../style/ProductPage.css";
 
+
 const ProductPage = () => {
     return (
         <section className="needflexin">
-            <div className="testingbox01">
-                <section className="ProductPageFilter">
-                    <h3>Filter</h3>
+            <section className="ProductsPage_Filter">
+                <h3>Filter</h3>
+                <div>
+                    <h3>Products</h3>
                     <div>
-                        <h3>Products</h3>
-                        <ul>
-                            <li>Books</li>
-                            <li>GamingParts</li>
-                            <li>Tours</li>
-                            <li>Games</li>
-                        </ul>
+
+                        <label>
+
+                            What do we eat?
+
+                            <select>
+
+                                <option value="fruit">Fruit</option>
+
+                                <option value="vegetable">Vegetable</option>
+
+                                <option value="meat">Meat</option>
+
+                            </select>
+
+                        </label>
+
                     </div>
 
-                    <div>
-                        <h4>Preise</h4>
-                        <ul>
-                            <li>0 - 10</li>
-                            <li>10- 50</li>
-                            <li>50 - 100</li>
-                            <li>100 - 500</li>
-                            <li>500 - 1000</li>
-                        </ul>
-                    </div>
-                </section>
+                    );
+                    <ul>
+                        <li>Books</li>
+                        <li>GamingParts</li>
+                        <li>Tours</li>
+                        <li>Games</li>
+                    </ul>
+                </div>
 
-            </div>
+                <div>
+                    <h4>Preise</h4>
+                    <ul>
+                        <li>0 - 10</li>
+                        <li>10- 50</li>
+                        <li>50 - 100</li>
+                        <li>100 - 500</li>
+                        <li>500 - 1000</li>
+                    </ul>
+                </div>
+            </section>
             <div className="testingbox02">
                 <div className="testingbox03"></div>
                 <div className="testingbox04">
