77cb9e6a6fa8264c2a520ffa31fceda89d6be57a
maxi <Maximilian.Autrum42@gmx.de>
2024-04-22 11:24:56 +0200

    update

--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -8,27 +8,6 @@ const ProductPage = () => {
                 <h3>Filter</h3>
                 <div>
                     <h3>Products</h3>
-                    <div>
-
-                        <label>
-
-                            What do we eat?
-
-                            <select>
-
-                                <option value="fruit">Fruit</option>
-
-                                <option value="vegetable">Vegetable</option>
-
-                                <option value="meat">Meat</option>
-
-                            </select>
-
-                        </label>
-
-                    </div>
-
-                    );
                     <ul>
                         <li>Books</li>
                         <li>GamingParts</li>
