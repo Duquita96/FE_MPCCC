a2d2a4118f7bda5b790858ad829a1a49751246b4
Pieter <135125619+PieterDCI@users.noreply.github.com>
2024-05-07 10:47:20 +0200

    add cart button component

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -1,12 +1,13 @@
 import { useContext } from 'react';
-import "../style/headerStyle.css";
 import SearchBar from "./Searchbar";
 import HeaderContext from '../context/HeaderContext';
 import AccountBtn from './cmpnts-header-buttons/AccountBtn';
 import LoginLogoutBtn from './cmpnts-header-buttons/LoginLogoutBtn';
+import CartBtn from './cmpnts-header-buttons/CartBtn';
+import "../style/headerStyle.css";
 
 const MainHeader = ({ windowWidth }) => {
-const {toggleCart,loginMsg} = useContext(HeaderContext)
+const {loginMsg} = useContext(HeaderContext)
 
 
   return (
@@ -14,7 +15,7 @@ const {toggleCart,loginMsg} = useContext(HeaderContext)
       {windowWidth > 768 && <div className="box-cart-login">logo</div>}
       {windowWidth > 768 && <AccountBtn />}
       <SearchBar />
-      {windowWidth > 768 && <div onClick={toggleCart} className="box-cart-login">cart</div>}
+      {windowWidth > 768 && <CartBtn />}
       {windowWidth > 768 && <LoginLogoutBtn />}
       {loginMsg === 1 && <div className='login-success'>You were successfully logged in</div>}
       {loginMsg === 2 && <div className='login-failed'>Login failed. Please try again</div>}
