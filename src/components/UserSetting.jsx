d1f9442a825ff012934d339e30e2e88f836473a0
Pieter <135125619+PieterDCI@users.noreply.github.com>
2024-05-12 09:07:41 +0200

    add UserInfo component and settings layout example

--- src/components/UserSetting.jsx
+++ src/components/UserSetting.jsx
@@ -1,38 +1,27 @@
 import { Link } from "react-router-dom";
-import "../style/userSettings.css";
+import UserInfo from "./cmpnts-settings-sections/UserInfo";
+import { useEffect, useState } from "react";
+
+
 
 const UserSettings = () => {
+
+  const [loginStatus, setLoginStatus] = useState(true);
+  const [adminStatus, setAdminStatus] = useState(false);
+
+  useEffect(()=> {
+    // get user by id
+    // set loginstatus
+    // set is admin or not
+    // log user out if token is not valid anymore
+  }, [])
+
   return (
     <div className="settings-container">
       <Link to={"/"} className="settings-home-btn">
         Back to Homepage
       </Link>
-      <div className="settings-user-info">
-        <h3>Your information:</h3>
-        <div>Name:</div>
-        <div>Surname: </div>
-        <div>Email:</div>
-        <div>Password: ****</div>
-        <button>Modify</button>
-        <button>Go to admin panel</button>
-      </div>
-      <div className="settings-user-admin">
-        <h3>Shipping addresses</h3>
-        <button>Add</button>
-      </div>
-      <div className="settings-user-options">
-        <h3>Options</h3>
-      </div>
-      <div className="settings-user-delete">
-        <h3>Delete Account</h3>
-        <button className="setting-del-btn">Delete</button>
-      </div>
-      <div className="settings-user-orders">
-        <h3>Current orders</h3>
-      </div>
-      <div className="settings-user-orders">
-        <h3>Order History</h3>
-      </div>
+      {loginStatus && <UserInfo />}
     </div>
   );
 };
