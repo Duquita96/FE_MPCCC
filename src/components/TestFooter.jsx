9837e21c3b6a40771b9c81626440a54309f5b39d
Pieter <135125619+PieterDCI@users.noreply.github.com>
2024-05-13 21:11:14 +0200

    add basic design of new footer

--- src/components/TestFooter.jsx
+++ src/components/TestFooter.jsx
@@ -1,12 +1,45 @@
-import '../style/footer.css'
+import "../style/footer.css";
 
 const TestFooter = () => {
-    return (
-        <div className="footer-container">
-            
+  return (
+    <div className="footer-container">
+      <div className="footer-social">
+        <div className="ft-social-icon pointer"></div>
+        <div className="ft-social-icon pointer"></div>
+        <div className="ft-social-icon pointer"></div>
+        <div className="ft-social-icon pointer"></div>
+      </div>
+      <div className="footer-main">
+        <div className="ft-box">
+          <p className="ft-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+            temporibus vitae ipsam exercitationem unde corrupti ea placeat
+            tempora. Distinctio voluptas vel id dignissimos quaerat, dicta ut
+            totam enim error molestiae!
+          </p>
         </div>
-    )
-}
+        <div className="ft-box">
+          <p className="ft-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+            temporibus vitae ipsam exercitationem unde corrupti ea placeat
+            tempora. Distinctio voluptas vel id dignissimos quaerat, dicta ut
+            totam enim error molestiae!
+          </p>
+        </div>
+        <div className="ft-box">
+          <p className="ft-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+            temporibus vitae ipsam exercitationem unde corrupti ea placeat
+            tempora. Distinctio voluptas vel id dignissimos quaerat, dicta ut
+            totam enim error molestiae!
+          </p>
+        </div>
+      </div>
+      <div className="footer-copyright">
+        <p>PMCCC Marketplace - All rights reserved. </p>
+      </div>
+    </div>
+  );
+};
 
-
-export default TestFooter
+export default TestFooter;
