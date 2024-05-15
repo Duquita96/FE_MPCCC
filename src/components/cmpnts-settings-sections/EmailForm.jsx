264174c7e7e065614c04fb567c67e61fb77b16be
Pieter <135125619+PieterDCI@users.noreply.github.com>
2024-05-15 18:39:47 +0200

    add changehandler to update user forms

--- src/components/cmpnts-settings-sections/EmailForm.jsx
+++ src/components/cmpnts-settings-sections/EmailForm.jsx
@@ -1,4 +1,21 @@
+import axios from "axios";
+import { useState } from "react";
+
 const EmailForm = ({cancel}) => {
+
+  const [newMail1, setNewMail1] = useState("");
+  const [newMail2, setNewMail2] = useState("");
+
+  const changeHandler = (e) => {
+    e.target.name === "newmail" ? setNewMail1(e.target.value) :
+      setNewMail2(e.target.value)
+  }
+
+  const patchEmail = () => {
+    // add patch
+  }
+
+
   return (
     <div className="modbox">
       <input
@@ -6,14 +23,18 @@ const EmailForm = ({cancel}) => {
         placeholder="new email"
         className="formInput"
         name="newmail"
+        value={newMail1}
+        onChange={changeHandler}
       />
       <input
         type="email"
         placeholder="confirm new email"
         className="formInput"
         name="newmail2"
+        value={newMail2}
+        onChange={changeHandler}
       />
-      <button className="modifyBtn pointer">Update</button>
+      <button className="modifyBtn pointer" onClick={patchEmail}>Update</button>
       <button className="modifyBtn pointer" onClick={cancel}>Cancel</button>
     </div>
   );
--- src/components/cmpnts-settings-sections/PasswordForm.jsx
+++ src/components/cmpnts-settings-sections/PasswordForm.jsx
@@ -1,4 +1,18 @@
+import axios from "axios";
+import { useState } from "react";
+
 const PasswordForm = ({cancel}) => {
+
+  const [oldPwd, setOldPwd] = useState("");
+  const [newPwd, setNewPwd] = useState("");
+  const [newPwd2, setNewPwd2] = useState("");
+
+  const changeHandler = (e) => {
+    e.target.name === "oldpwd" ? setOldPwd(e.target.value) :
+      e.target.name === "newpwd" ? setNewPwd (e.target.value) :
+      setNewPwd2(e.target.value)
+  }
+
     return (
         <div className="modbox">
           <input
@@ -6,18 +20,24 @@ const PasswordForm = ({cancel}) => {
             placeholder="current password"
             className="formInput"
             name="oldpwd"
+            value={oldPwd}
+            onChange={changeHandler}
           />
           <input
             type="password"
             placeholder="new password"
             className="formInput"
             name="newpwd"
+            value={newPwd}
+            onChange={changeHandler}
           />
           <input
             type="password"
             placeholder="confirm new password"
             className="formInput"
             name="newpwd2"
+            value={newPwd2}
+            onChange={changeHandler}
           />
           <button className="modifyBtn pointer">Update</button>
           <button className="modifyBtn pointer" onClick={cancel}>Cancel</button>
