a346d3e651cdfec782274e406fb32ddb21a34cfa
Claudia <duquita96@gmail.com>
2024-05-15 15:28:17 +0200

    added carrousel with all cards

---
+++ src/components/cmpnts-productPreview/CardsCollection.jsx
@@ -0,0 +1,117 @@
+// import { useEffect, useState, useRef } from 'react';
+// import CardM from './Card.jsx';
+// import { ProductPreviewClick } from './ProductPreview-Click.jsx';
+
+// export const CardsCollection = () => {
+//   const [productsData, setProductsData] = useState([]);
+//   const [activeIndex, setActiveIndex] = useState(0);
+//   const cardsContainerRef = useRef(null);
+
+//   useEffect(() => {
+//     fetch('http://localhost:8000/api/v1/books')
+//       .then(res => res.json())
+//       .then(data => {
+//         const randomizedData = data.data;
+//         setProductsData(randomizedData);
+//       })
+//       .catch(err => console.log(err));
+//   }, []);
+
+//   const handleWheel = (event) => {
+//     if (event.deltaY > 0) {
+//       next();
+//     } else {
+//       prev();
+//     }
+//   };
+
+//   const next = () => {
+//     setActiveIndex((prevIndex) => (prevIndex + 1) % productsData.length);
+//   };
+
+//   const prev = () => {
+//     setActiveIndex((prevIndex) => (prevIndex - 1 + productsData.length) % productsData.length);
+//   };
+
+//   const getCardClass = (index) => {
+//     console.log("index: ", index, " activeIndex: ", activeIndex);
+//     if (index === activeIndex) {
+//       console.log("act");
+//       return 'act';
+//     }
+//     if (index === (activeIndex - 1 + productsData.length) % productsData.length){
+//       console.log("actprev");
+//       return 'actprev';
+//     }
+//     if (index === (activeIndex + 1) % productsData.length){
+//       console.log("actnext");
+//       return 'actnext';
+//     }
+//     /* if (index === (activeIndex - 1 + productsData.length) % productsData.length) { */
+//     if (index === (activeIndex + 2) % productsData.length) {
+//    console.log("next");
+//       return 'next';
+//     }
+//     /* if (index === (activeIndex - 1 + productsData.length) % productsData.length) { */
+//     if (index === (activeIndex - 2 + productsData.length) % productsData.length) {
+//    console.log("prev");
+//       return 'prev';
+//     }
+//     console.log("hide");
+//     return 'hide';
+//   };
+
+//   return (
+//     <div id='cards-collection' onWheel={handleWheel}>
+//       <ul id="cards-container" className="list" ref={cardsContainerRef}>
+//         {productsData.map((card, index) => (
+//           <li key={card._id} className={getCardClass(index)}>
+//             <ProductPreviewClick id={card._id} productType={card.productType}>
+//               <div className='card-container pointer'>
+//                 <CardM card={card} />
+//               </div>
+//             </ProductPreviewClick>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default CardsCollection;
+
+import { useEffect, useState } from 'react';
+import CardM from './Card.jsx';
+import { ProductPreviewClick } from './ProductPreview-Click.jsx';
+
+export const CardsCollection = () => {
+  const [productsData, setProductsData] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8000/api/v1/books')
+      .then(res => res.json())
+      .then(data => {
+        const randomizedData = data.data;
+        setProductsData(randomizedData);
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+  return (
+    <div id='cards-collection'>
+      <ul id="cards-container" className="list">
+        {productsData.map((card) => (
+          <li key={card._id}>
+            <ProductPreviewClick id={card._id} productType={card.productType}>
+              <div className='card-container pointer'>
+                <CardM card={card} />
+              </div>
+            </ProductPreviewClick>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default CardsCollection;
--- src/components/cmpnts-productPreview/ProductsPreview.jsx
+++ src/components/cmpnts-productPreview/ProductsPreview.jsx
@@ -1,21 +1,82 @@
+// import CardsCollection from "./CardsCollection.jsx";
+// import '../../style/ProductsPreview.css'
+// import ToursCollection from './ToursCollection.jsx'
+// import PcPartsCollection from './PcPartsCollection.jsx';
+// import VideoGamesCollection from './VideoGamesCollection.jsx'
+// /* import {Link} from 'react-router-dom'; */
+
+
+// export const ProductsPreview = () => {
+
+//     return (
+//         <div id="products-preview-section">
+//             <ToursCollection />
+//             <div>
+//                 <CardsCollection />
+//                 <PcPartsCollection />
+//                 <VideoGamesCollection />
+//             </div>
+
+//         </div>
+//     );
+// }
+
+// export default ProductsPreview;
+
+import { useRef, useEffect } from 'react';
 import BooksCollection from "./BooksCollection.jsx";
 import '../../style/ProductsPreview.css'
 import ToursCollection from './ToursCollection.jsx'
 import PcPartsCollection from './PcPartsCollection.jsx';
 import VideoGamesCollection from './VideoGamesCollection.jsx'
-/* import {Link} from 'react-router-dom'; */
-
 
 export const ProductsPreview = () => {
+    const collectionsContainerRef = useRef(null);
+
+    // Asumiendo que cada colección tiene el mismo ancho fijo
+    const collectionWidth = 300; // Reemplaza con el ancho real de tus colecciones
+
+    const handleWheel = (event) => {
+        if (collectionsContainerRef.current) {
+            const container = collectionsContainerRef.current;
+            const scrollLeft = container.scrollLeft;
+            const maxScrollLeft = container.scrollWidth - container.clientWidth;
+
+            if (event.deltaY > 0) {
+                if (scrollLeft >= maxScrollLeft - collectionWidth) {
+                    // Si estamos al final, volvemos al principio sin animación
+                    container.scrollTo({ left: 0, behavior: 'auto' });
+                } else {
+                    container.scrollTo({ left: scrollLeft + collectionWidth, behavior: 'smooth' });
+                }
+            } else {
+                if (scrollLeft <= collectionWidth) {
+                    // Si estamos al principio, saltamos al final sin animación
+                    container.scrollTo({ left: maxScrollLeft, behavior: 'auto' });
+                } else {
+                    container.scrollTo({ left: scrollLeft - collectionWidth, behavior: 'smooth' });
+                }
+            }
+        }
+    };
+
+    // Ajusta el desplazamiento inicial para el duplicado del primer elemento
+    useEffect(() => {
+        if (collectionsContainerRef.current) {
+            collectionsContainerRef.current.scrollLeft = collectionWidth;
+        }
+    }, []);
 
     return (
         <div id="products-preview-section">
             <ToursCollection />
-            <BooksCollection />
-            <PcPartsCollection/>
-            <VideoGamesCollection/>
+            <div id="collections-container" ref={collectionsContainerRef} onWheel={handleWheel} >
+                <VideoGamesCollection />
+                <BooksCollection />
+                <PcPartsCollection />
+                <VideoGamesCollection />
 
-
+            </div>
         </div>
     );
 }
