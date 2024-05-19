d787cb0c2fac9882b6b8120ab8c8a2b836c6ac14
Pieter <135125619+PieterDCI@users.noreply.github.com>
2024-05-19 20:40:49 +0200

    add mobile navbar to user account page

--- src/components/pages/UserAccountPage.jsx
+++ src/components/pages/UserAccountPage.jsx
@@ -3,16 +3,20 @@ import { useContext } from "react";
 import Login from "../Login.jsx";
 import Cart from "../Cart.jsx";
 import HeaderContext from "../../context/HeaderContext.jsx";
+import WidthContext from '../../context/WidthContext.jsx';
 import UserSettings from "../cmpnts-settings-sections/UserSetting.jsx";
+import MobileNavBar from "../cmpnts-homepage-navbars/MobileNavBar.jsx";
 import PageWrapper from "../PageWrapper.jsx";
 
 const UserAccount = () => {
   const { showLogin, showCart } = useContext(HeaderContext);
+  const { windowWidth } = useContext(WidthContext)
 
   return (
     <PageWrapper>
       {showLogin && <Login />}
       {showCart && <Cart />}
+      {windowWidth <= 768 && <MobileNavBar  />}
       <Link to={"/"} className="settings-home-btn">
         Back 
       </Link>
