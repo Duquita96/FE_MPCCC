ffe80a36700ce607f2de87371900817a2005bf25
Pieter <135125619+PieterDCI@users.noreply.github.com>
2024-05-22 15:59:58 +0200

    link searchbar data to backend fetch

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,25 +1,77 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+// import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import "../style/headerStyle.css";
-import dataObj from "./data/searchData.js";
 
 const SearchBar = () => {
-  const items = Object.keys(dataObj);
   const [inputStr, setInputStr] = useState("");
+  const [searchData, setSearchData] = useState([]);
+  const [searchDataType, setSearchDataType] = useState({});
 
-  const handleChange = (e) => {
-    setInputStr(e.target.value);
+  const updateSearchData = (array) => {
+    setSearchData(array);
   };
-  const clear = () => {
-    setInputStr("");
+  const updateSearchTypes = (obj) => {
+    setSearchDataType(obj);
   };
+
+  let searchObj = {
+    Books: { type: "Category" },
+    Games: { type: "Category" },
+    PcParts: { type: "Category" },
+    Tours: { type: "Category" },
+  };
+
+  useEffect(() => {
+    if (searchData.length === 0) {
+      axios
+        .get("http://localhost:8000/api/v1/resources/list")
+        .then((res) => {
+          const fetchArray = res.data.data;
+          console.log(fetchArray);
+          fetchArray.forEach((obj) => {
+            switch (obj.productType) {
+              case "books":
+                searchObj[obj.name] = { type: "Book", id: obj._id };
+                break;
+              case "video-games":
+                searchObj[obj.name] = { type: "Games", id: obj._id };
+                break;
+              case "pc-parts":
+                searchObj[obj.name] = { type: "PC Parts", id: obj._id };
+                break;
+              case "tours":
+                searchObj[obj.name] = { type: "Tours", id: obj._id };
+                break;
+            }
+          });
+          updateSearchTypes(searchObj);
+          updateSearchData(Object.keys(searchObj));
+        })
+        .catch((err) => console.log(err));
+    }
+  }, []);
+
+  const handleChange = (e) => {setInputStr(e.target.value)};
+  const clear = () => {setInputStr("")};
+
+  // const navigate = useNavigate();
+  // const moveTo = (url) => {navigate(url)};
 
   const searchSelector = (e) => {
-    const selectedSearch = e.target.innerText.split("|");
-    const searchInput = selectedSearch[0].toLowerCase();
-    const searchType = selectedSearch[1].toLowerCase();
 
-    // Add fetching info where needed
-    // Add redirecting of user based on selection
+    // const selectedId = e.target.id;
+    // const selectedCategory = e.target.innerText.split("|")[0];
+
+    // console.log(selectedId);
+    // console.log(selectedCategory);
+
+    // if (!selectedId) {
+    //   if (selectedCategory === "Games") {moveTo("/filter-page/video-game")};
+    //   if (selectedCategory === "Books") {moveTo("/filter-page/book")};
+    //   if (selectedCategory === "PcParts") {moveTo("/filter-page/pc-part")};
+    //   if (selectedCategory === "Tours") {moveTo("/filter-page/tours")};
+    // }
   };
 
   return (
@@ -37,15 +89,15 @@ const SearchBar = () => {
         </form>
       </div>
       <ul className="search-result-list">
-        {items
+        {searchData
           .filter((item) =>
             inputStr.toLowerCase() === ""
               ? ""
               : item.toLowerCase().includes(inputStr.toLowerCase())
           )
           .map((item) => (
-            <li key={item} onClick={searchSelector}>
-              {item} |<span className="searchType">{dataObj[item].type}</span>
+            <li key={item} id={searchDataType[item].id} onClick={searchSelector}>
+              {item} | {searchDataType[item].type}
             </li>
           ))}
       </ul>
