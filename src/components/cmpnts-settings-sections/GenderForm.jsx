c058b474ff555a9afee085e550cdf41dffb3e944
Carlos Mertens <mertenscarlos3000@gmail.com>
2024-05-28 10:43:56 +0200

    Add toggle for newsletter option

--- src/components/cmpnts-settings-sections/GenderForm.jsx
+++ src/components/cmpnts-settings-sections/GenderForm.jsx
@@ -1,31 +1,33 @@
 import { UserContext } from '../../context/UserContextProvider.jsx';
-import { useState, useContext } from "react";
-import patchFunction from "../../utils/patch";
+import { useState, useContext } from 'react';
+import patchFunction from '../../utils/patch';
 
 const GenderForm = ({ cancel }) => {
   const { userDispatch } = useContext(UserContext);
-  const [gen, setGen] = useState("");
+  const [gen, setGen] = useState('');
 
-  const changeGender = (e) => {setGen(e.target.value)};
+  const changeGender = e => {
+    setGen(e.target.value);
+  };
 
   const patchGender = () => {
-        patchFunction({ gender: gen });
-        userDispatch({ type: "gender", gender: gen });
-        cancel();
-      };
+    patchFunction({ gender: gen });
+    userDispatch({ type: 'gender', gender: gen });
+    cancel();
+  };
 
   return (
-    <div className="modbox">
-      <select name="genders" id="gender-select" onChange={changeGender}>
-        <option value="">Please select your gender</option>
-        <option value="male">Male</option>
-        <option value="female">Female</option>
-        <option value="other">Other</option>
+    <div className='modbox'>
+      <select name='genders' id='gender-select' onChange={changeGender}>
+        <option value=''>Please select your gender</option>
+        <option value='male'>Male</option>
+        <option value='female'>Female</option>
+        <option value='other'>Other</option>
       </select>
-      <button className="modifyBtn pointer" onClick={patchGender}>
+      <button className='modifyBtn pointer' onClick={patchGender}>
         Update
       </button>
-      <button className="modifyBtn pointer" onClick={cancel}>
+      <button className='modifyBtn pointer' onClick={cancel}>
         Cancel
       </button>
     </div>
--- src/context/UserContextProvider.jsx
+++ src/context/UserContextProvider.jsx
@@ -1,4 +1,4 @@
-import { useReducer, useState, createContext } from "react";
+import { useReducer, useState, createContext } from 'react';
 
 export const UserContext = createContext();
 
@@ -6,61 +6,59 @@ const initialState = {};
 
 const userReducer = (state, action) => {
   switch (action.type) {
-    case "email":
+    case 'email':
       return { ...state, email: action.email };
-    case "firstName":
+    case 'firstName':
       return { ...state, firstName: action.firstName };
-    case "gender":
+    case 'gender':
       return { ...state, gender: action.gender };
-    case "home":
+    case 'home':
       return { ...state, homeAddress: action.homeAddress };
-    case "lastName":
+    case 'lastName':
       return { ...state, lastName: action.lastName };
-    case "news":
-      return { ...state, newsLetter: action.newsLetter };
-    case "orders":
+    case 'news':
+      return { ...state, newsletter: action.newsletter };
+    case 'orders':
       return { ...state, orders: action.orders };
-    case "shipping":
+    case 'shipping':
       return { ...state, shippingAddress: action.shippingAddress };
-    case "_id":
+    case '_id':
       return { ...state, id: action._id };
-    case "newUser":
+    case 'newUser':
       return { ...state, ...action.payload };
-    case "noUser":
+    case 'noUser':
       return { initialState };
     default:
       return state;
   }
 };
 
-
-
 /** Provides information about the logged in user to the entire app */
 const UserContextProvider = ({ children }) => {
-
   const [userState, userDispatch] = useReducer(userReducer, initialState);
   const [loggedIn, setLoggedIn] = useState(false);
 
-  const loginON = () => {setLoggedIn(true)};
-  const loginOFF = () => {setLoggedIn(false)};
+  const loginON = () => {
+    setLoggedIn(true);
+  };
+  const loginOFF = () => {
+    setLoggedIn(false);
+  };
 
-  const addNewUser = (obj) => {
-    userDispatch({type: "newUser",payload: obj});
-    loginON()
+  const addNewUser = obj => {
+    userDispatch({ type: 'newUser', payload: obj });
+    loginON();
   };
 
   const resetUser = () => {
-    userDispatch({ type: "noUser" });
+    userDispatch({ type: 'noUser' });
     loginOFF();
     localStorage.clear();
   };
 
-  
-
   return (
     <UserContext.Provider
-      value={{ userState, userDispatch, addNewUser, resetUser, loggedIn }}
-    >
+      value={{ userState, userDispatch, addNewUser, resetUser, loggedIn }}>
       {children}
     </UserContext.Provider>
   );
