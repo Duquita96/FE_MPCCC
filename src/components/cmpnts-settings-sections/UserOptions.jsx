da5680c14027ceb526205e08104f970f04fb11d3
Pieter <135125619+PieterDCI@users.noreply.github.com>
2024-05-31 10:52:20 +0200

    add feedback message functionality to newsletter toggle

--- src/components/cmpnts-settings-sections/UserOptions.jsx
+++ src/components/cmpnts-settings-sections/UserOptions.jsx
@@ -1,5 +1,6 @@
 import { useContext, useState } from 'react';
 import { UserContext } from '../../context/UserContextProvider';
+import { HeaderContext } from "../../context/HeaderContextProvider.jsx";
 import { ToggleSwitch } from '../ToggleSwitch';
 import patchFunction from '../../utils/patch';
 
@@ -10,9 +11,15 @@ const UserOptions = () => {
   const [isNewsletter, setIsNewsletter] = useState(false);
   console.log({ isNewsletter });
 
+  const { toggleFeedbackMsg } = useContext(HeaderContext);
+
+  /** Displays a confirmation of un/subscribe for 3 seconds */
+  const feedbackMsg = (number) => {toggleFeedbackMsg(number); setTimeout(() => {toggleFeedbackMsg(0)}, 3000)}
+  // 5 = subscribe - 6 = unsubscribe
+
   const onChange = async () => {
-    if (isNewsletter) alert('You have unbsubscribed from our newsletter');
-    else alert('Thanks for subscribing to our newsletter!');
+    if (isNewsletter) feedbackMsg(5);
+    else feedbackMsg(6);
 
     await patchFunction({ newsletter: !isNewsletter }).then(() => {
       userDispatch({ type: 'news', newsletter: !isNewsletter });
@@ -21,7 +28,7 @@ const UserOptions = () => {
   };
   return (
     <div className='settings-user-options'>
-      <h3>Options</h3>
+      <h3 className='h3settings'>Options</h3>
       <div className='delBox'>
         <p>Receive newsletter?</p>
         <ToggleSwitch
