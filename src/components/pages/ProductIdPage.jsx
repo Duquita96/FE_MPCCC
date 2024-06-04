71011486abed771bd9ba6146d04bad4c50807726
Carlos Mertens <mertenscarlos3000@gmail.com>
2024-06-04 13:46:45 +0200

    Disable payment icons

--- src/components/pages/ProductIdPage.jsx
+++ src/components/pages/ProductIdPage.jsx
@@ -1,24 +1,24 @@
 //React libraries and others
-import { useParams, useNavigate } from "react-router-dom";
-import { useEffect, useState, useContext } from "react";
-import PropTypes from "prop-types";
-import Zoom from "react-medium-image-zoom";
-import "react-medium-image-zoom/dist/styles.css";
+import { useParams, useNavigate } from 'react-router-dom';
+import { useEffect, useState, useContext } from 'react';
+import PropTypes from 'prop-types';
+import Zoom from 'react-medium-image-zoom';
+import 'react-medium-image-zoom/dist/styles.css';
 
 //Components
-import Login from "../Login.jsx";
-import Cart from "../Cart.jsx";
-import { HeaderContext } from "../../context/HeaderContextProvider.jsx";
-import { cartContext } from "../../context/CartContextProvider.jsx";
-import { WidthContext } from "../../context/WidthContextProvider.jsx";
-import { UserContext } from "../../context/UserContextProvider.jsx";
-import { getImagePath } from "../cmpnts-productPreview/GenericCard.jsx";
-import Comments from "../cmpnts-productPreview/Comments.jsx";
-import PageWrapper from "../PageWrapper.jsx";
-import MobileNavBar from "../../components/cmpnts-homepage-navbars/MobileNavBar.jsx";
+import Login from '../Login.jsx';
+import Cart from '../Cart.jsx';
+import { HeaderContext } from '../../context/HeaderContextProvider.jsx';
+import { cartContext } from '../../context/CartContextProvider.jsx';
+import { WidthContext } from '../../context/WidthContextProvider.jsx';
+import { UserContext } from '../../context/UserContextProvider.jsx';
+import { getImagePath } from '../cmpnts-productPreview/GenericCard.jsx';
+import Comments from '../cmpnts-productPreview/Comments.jsx';
+import PageWrapper from '../PageWrapper.jsx';
+import MobileNavBar from '../../components/cmpnts-homepage-navbars/MobileNavBar.jsx';
 
 //css
-import "../../style/productIdPage.css";
+import '../../style/productIdPage.css';
 
 //icons
 import {
@@ -27,8 +27,8 @@ import {
   RiMastercardLine,
   RiBankFill,
   RiCalendarScheduleFill,
-} from "react-icons/ri";
-import { BsFillArrowLeftSquareFill } from "react-icons/bs";
+} from 'react-icons/ri';
+import { BsFillArrowLeftSquareFill } from 'react-icons/bs';
 
 export const ProductIdPage = ({ productType }) => {
   const { id } = useParams();
@@ -44,29 +44,35 @@ export const ProductIdPage = ({ productType }) => {
   const { windowWidth } = useContext(WidthContext);
   const { loggedIn } = useContext(UserContext);
 
-    /** Displays a confirmation of succesfull add-to-cart for 3 seconds */
-    const confirmAddToCart = () => {toggleFeedbackMsg(3); setTimeout(() => {toggleFeedbackMsg(0)}, 3000)}
+  /** Displays a confirmation of succesfull add-to-cart for 3 seconds */
+  const confirmAddToCart = () => {
+    toggleFeedbackMsg(3);
+    setTimeout(() => {
+      toggleFeedbackMsg(0);
+    }, 3000);
+  };
 
-
-  const handleGoBack = () => {navigate(-1)};
+  const handleGoBack = () => {
+    navigate(-1);
+  };
 
   const addItem = () => {
     let objToAdd;
-    productType === "books"
+    productType === 'books'
       ? (objToAdd = {
           productId: book._id,
           name: book.name,
           productType: book.productType,
           price: book.price,
         })
-      : productType === "tours"
+      : productType === 'tours'
       ? (objToAdd = {
           productId: tour._id,
           name: tour.name,
           productType: tour.productType,
           price: tour.price,
         })
-      : productType === "video-games"
+      : productType === 'video-games'
       ? (objToAdd = {
           productId: videoGames._id,
           name: videoGames.name,
@@ -83,7 +89,7 @@ export const ProductIdPage = ({ productType }) => {
     confirmAddToCart();
   };
 
-  productType === "tours";
+  productType === 'tours';
 
   useEffect(() => {
     const fetchData = async () => {
@@ -93,19 +99,19 @@ export const ProductIdPage = ({ productType }) => {
         );
         const data = await response.json();
         switch (productType) {
-          case "books":
+          case 'books':
             setBook(data.data);
             setImgPath(getImagePath(data.data));
             break;
-          case "tours":
+          case 'tours':
             setTour(data.data);
             setImgPath(getImagePath(data.data));
             break;
-          case "pc-parts":
+          case 'pc-parts':
             setPcParts(data.data);
             setImgPath(getImagePath(data.data));
             break;
-          case "video-games":
+          case 'video-games':
             setVideoGames(data.data);
             setImgPath(getImagePath(data.data));
             break;
@@ -120,116 +126,112 @@ export const ProductIdPage = ({ productType }) => {
     fetchData();
   }, [id, productType]);
 
-
-  const renderProductDetails = (book) => (
-    <div className="product_section">
-      <div className="imgContainer">
+  const renderProductDetails = book => (
+    <div className='product_section'>
+      <div className='imgContainer'>
         <Zoom zoomMargin={10}>
-          <img src={imgPath} alt={"productImage"} className="zoomImg" />
+          <img src={imgPath} alt={'productImage'} className='zoomImg' />
         </Zoom>
       </div>
-      <div className="product">
+      <div className='product'>
         <>
-          <p id="p-author" className="p-height">
+          <p id='p-author' className='p-height'>
             <strong>Author</strong>: {book.author}
           </p>
-          <p id="p-genre" className="p-height">
+          <p id='p-genre' className='p-height'>
             <strong>Genre</strong>: {book.genre}
           </p>
-          <p id="p-price" className="p-height">
+          <p id='p-price' className='p-height'>
             <strong>Price</strong>: {book.price} €
           </p>
-          <p id="p-pages" className="p-height">
+          <p id='p-pages' className='p-height'>
             <strong>Pages</strong>: {book.pages}
           </p>
-          <p id="p-description" className="description-box">
+          <p id='p-description' className='description-box'>
             <strong>Description</strong>: {book.description}
           </p>
           <div>
             <button
-              className="add-to-crt"
+              className='add-to-crt'
               disabled={!loggedIn}
               onClick={addItem}
-              style={{ backgroundColor: loggedIn ? null : "grey" }}
-            >
-              {loggedIn ? "Add to Cart" : "Not logged in"}
+              style={{ backgroundColor: loggedIn ? null : 'grey' }}>
+              {loggedIn ? 'Add to Cart' : 'Not logged in'}
             </button>
           </div>
         </>
       </div>
     </div>
   );
-  const renderServiceDetails = (tour) => (
-    <div className="product_section">
-      <div className="imgContainer">
+  const renderServiceDetails = tour => (
+    <div className='product_section'>
+      <div className='imgContainer'>
         <Zoom zoomMargin={10}>
-          <img src={imgPath} alt={"productImage"} className="zoomImg" />
+          <img src={imgPath} alt={'productImage'} className='zoomImg' />
         </Zoom>
       </div>
 
-      <div className="product">
-        <p id="t-title" className="p-height">
+      <div className='product'>
+        <p id='t-title' className='p-height'>
           <strong>Name</strong>: {tour.name}
         </p>
-        <p id="t-author" className="p-height">
+        <p id='t-author' className='p-height'>
           <strong>Group Size</strong>: {tour.maxGroupSize}
         </p>
-        <p id="t-genre" className="p-height">
+        <p id='t-genre' className='p-height'>
           <strong>Difficulty</strong>: {tour.difficulty}
         </p>
-        <p id="t-price" className="p-height">
+        <p id='t-price' className='p-height'>
           <strong>Price</strong>: {tour.price} €
         </p>
-        <p id="t-pages" className="p-height">
+        <p id='t-pages' className='p-height'>
           <strong>Duration</strong>: {tour.duration}h
         </p>
-        <p id="t-description" className="description-box">
+        <p id='t-description' className='description-box'>
           <strong>Description</strong>:{tour.description}
         </p>
-        <div id="add-button-container">
+        <div id='add-button-container'>
           <button
-            className="add-to-crt"
+            className='add-to-crt'
             disabled={!loggedIn}
             onClick={addItem}
-            style={{ backgroundColor: loggedIn ? null : "grey" }}
-          >
-            {loggedIn ? "Reserve" : "Not logged in"}
+            style={{ backgroundColor: loggedIn ? null : 'grey' }}>
+            {loggedIn ? 'Reserve' : 'Not logged in'}
           </button>
         </div>
       </div>
     </div>
   );
-  const renderPcDetails = (pcParts) => {
+  const renderPcDetails = pcParts => {
     return (
-      <div className="product_section">
-        <div className="imgContainer">
+      <div className='product_section'>
+        <div className='imgContainer'>
           <Zoom zoomMargin={10}>
-            <img src={imgPath} alt={"productImage"} className="zoomImg" />
+            <img src={imgPath} alt={'productImage'} className='zoomImg' />
           </Zoom>
         </div>
 
-        <div className="product">
+        <div className='product'>
           <>
-            <p id="p-author" className="p-height">
+            <p id='p-author' className='p-height'>
               <strong>Brand</strong>: {pcParts.brand}
             </p>
-            <p id="p-genre" className="p-height">
+            <p id='p-genre' className='p-height'>
               <strong>Category</strong>: {pcParts.category}
             </p>
-            <p id="p-price" className="p-height">
+            <p id='p-price' className='p-height'>
               <strong>Price</strong>: {pcParts.price} €
             </p>
-            <p id="p-description" className="description-box">
+            <p id='p-description' className='description-box'>
               <strong>Description</strong>: {pcParts.description}
             </p>
-            <div className="add-to-crt-container">
+            <div className='add-to-crt-container'>
               <button
-                className="add-to-crt"
+                className='add-to-crt'
                 disabled={!loggedIn}
                 onClick={addItem}
-                style={{ backgroundColor: loggedIn ? null : "grey" }}
-              >
-                {loggedIn ? "Add to Cart" : "Not logged in"}
+                style={{ backgroundColor: loggedIn ? null : 'grey' }}>
+                {loggedIn ? 'Add to Cart' : 'Not logged in'}
               </button>
             </div>
           </>
@@ -238,34 +240,33 @@ export const ProductIdPage = ({ productType }) => {
     );
   };
 
-  const renderVideoGamesDetails = (videoGames) => {
+  const renderVideoGamesDetails = videoGames => {
     return (
-      <div className="product_section">
-        <div className="imgContainer">
+      <div className='product_section'>
+        <div className='imgContainer'>
           <Zoom zoomMargin={10}>
-            <img src={imgPath} alt={"productImage"} className="zoomImg" />
+            <img src={imgPath} alt={'productImage'} className='zoomImg' />
           </Zoom>
         </div>
 
-        <div className="product">
+        <div className='product'>
           <>
-            <p id="p-title" className="p-height">
+            <p id='p-title' className='p-height'>
               <strong>Name</strong>: {videoGames.name}
             </p>
-            <p id="p-price" className="p-height">
+            <p id='p-price' className='p-height'>
               <strong>Price</strong>: {videoGames.price} €
             </p>
-            <p id="p-description" className="description-box">
+            <p id='p-description' className='description-box'>
               <strong>Description</strong>: {videoGames.description}
             </p>
             <div>
               <button
-                className="add-to-crt"
+                className='add-to-crt'
                 disabled={!loggedIn}
                 onClick={addItem}
-                style={{ backgroundColor: loggedIn ? null : "grey" }}
-              >
-                {loggedIn ? "Add to Cart" : "Not logged in"}
+                style={{ backgroundColor: loggedIn ? null : 'grey' }}>
+                {loggedIn ? 'Add to Cart' : 'Not logged in'}
               </button>
             </div>
           </>
@@ -280,12 +281,12 @@ export const ProductIdPage = ({ productType }) => {
         {showLogin && <Login />}
         {showCart && <Cart />}
         {windowWidth <= 768 && <MobileNavBar />}
-        <div id="product_description">
-          <div className="button_container">
-            <button className="filter-goBack-button" onClick={handleGoBack}>
-              <BsFillArrowLeftSquareFill id="arrowIcon" />
+        <div id='product_description'>
+          <div className='button_container'>
+            <button className='filter-goBack-button' onClick={handleGoBack}>
+              <BsFillArrowLeftSquareFill id='arrowIcon' />
             </button>
-            <div id="product-title">
+            <div id='product-title'>
               {book
                 ? book.name
                 : tour
@@ -294,10 +295,10 @@ export const ProductIdPage = ({ productType }) => {
                 ? pcParts.name
                 : videoGames
                 ? videoGames.name
-                : "Item Name"}
+                : 'Item Name'}
             </div>
           </div>
-          <div className="product-main">
+          <div className='product-main'>
             {book ? (
               renderProductDetails(book)
             ) : tour ? (
@@ -310,28 +311,33 @@ export const ProductIdPage = ({ productType }) => {
               <div>insert 404</div>
             )}
           </div>
-          <div id="buy_section">
+          <div id='buy_section'>
             <div>
-              <RiPaypalFill id="paypalIcon" className="buyIcon" />
+              <RiPaypalFill id='paypalIcon' className='buyIcon' color='gray' />
             </div>
             <div>
-              <RiVisaFill id="visaIcon" className="buyIcon" />
+              <RiVisaFill id='visaIcon' className='buyIcon' color='gray' />
             </div>
             <div>
-              <RiMastercardLine id="masterIcon" className="buyIcon" />
+              <RiMastercardLine
+                id='masterIcon'
+                className='buyIcon'
+                color='gray'
+              />
             </div>
             <div>
-              <RiBankFill id="bankIcon" className="buyIcon" />
+              <RiBankFill id='bankIcon' className='buyIcon' color='gray' />
             </div>
             <div>
               <RiCalendarScheduleFill
-                id="buyNowCalendarIcon"
-                className="buyIcon"
+                id='buyNowCalendarIcon'
+                className='buyIcon'
+                color='gray'
               />
             </div>
           </div>
 
-          <div id="comments-section">
+          <div id='comments-section'>
             <Comments productType={productType} />
           </div>
         </div>
